a484e419c5275bed3b4fa6c4dafb3fbb0f8b364b
Aflu <mohamedaflah186@gmail.com>
2024-07-31 20:40:37 +0530

    feat: origin added

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000","https://taskmngment-task.vercel.app"],
     credentials: true,
   })
 );
